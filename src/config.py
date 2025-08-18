f9c9320aa5113be72812cd8cb6f496bf85c99a64
jackewings <jackewings@comcast.net>
2025-08-18 12:52:35 -0500

    Add API fetch, week calculation, and CSV saving

---
+++ src/config.py
@@ -0,0 +1,3 @@
+CURRENT_WEEK = 1
+PASSWORD = "demo123"  # replace with st.secrets in deployment
+DATA_FILE = "data/picks.csv"
---
+++ test_data.py
@@ -0,0 +1,5 @@
+from src.data import get_weekly_spreads
+
+if __name__ == "__main__":
+    spreads = get_weekly_spreads()
+    print(spreads)
