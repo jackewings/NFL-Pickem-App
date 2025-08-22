a2607fbab9d576eac2448365bbeedd123ac10e78
jackewings <jackewings@comcast.net>
2025-08-22 11:04:38 -0500

    Update workflow and core files for automated updates

--- src/core/update_data.py
+++ src/core/update_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 from pathlib import Path
 from datetime import datetime
 import pytz
-import core.data as data  # Changed from 'from src import data'
+from . import data
 import logging
 
 # Set up logging
